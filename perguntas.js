f8fe6a903c50c40e54843530c8cd843325766492
caiohenriqu3 <fushiguro.topone@gmail.com>
2024-11-13 08:30:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    'Fato interessante',
+    'O que é acontece se você comer 1 milhão de bananas',
+    'você morre!! :D'
+)
+
+
+criaCartao(
+    'conhecimento interessante mais inutil',
+    'O que é um graveto?',
+    'Um graveto é um pedaço de pau'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz whatsapp em Inglês?',
+    'fiu fiu fiufiufiu (olha mensagem)'
+)
